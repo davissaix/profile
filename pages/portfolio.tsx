396378fbcbde117675878555065b7b413bec1977
David AVILA ALVAREZ <davissaix@gmail.com>
2023-08-22 12:12:15 -0500

    images

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../src/app/layout';
+import Image from 'next/image'; // Import the Image component
 
 const Portfolio: React.FC = () => {
   return (
@@ -10,7 +11,7 @@ const Portfolio: React.FC = () => {
 
           {/* Project 1: Multi-language Translator */}
           <div className="project bg-gray-100 p-6 rounded-lg shadow-md">
-            <img src="/trans.png" alt="Multi-language Translator Preview" className="w-full mb-4 rounded"/>
+            <Image src="/trans.png" alt="Multi-language Translator Preview" width={500} height={300} className="w-full mb-4 rounded"/>
             <h2 className="text-2xl font-semibold mb-4">Multi-language Translator</h2>
             <p className="mb-4 text-justify">A web app that translates text into multiple languages at once. Uses asynchronous API calls for translations.</p>
             <a href="https://polytranslate.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">View Project</a>
@@ -18,7 +19,7 @@ const Portfolio: React.FC = () => {
 
           {/* Project 2: 3D Experience with Three.js */}
           <div className="project bg-gray-100 p-6 rounded-lg shadow-md">
-            <img src="/cora.png" alt="3D Web Experience Preview" className="mb-4 rounded"/>
+            <Image src="/cora.png" alt="3D Web Experience Preview" width={500} height={300} className="mb-4 rounded"/>
             <h2 className="text-2xl font-semibold mb-4">3D Web Experience</h2>
             <p className="mb-4 text-justify">An interactive 3D environment created with Three.js. Features GPU-accelerated graphics rendering with WebGL.</p>
             <a href="https://sanvalentine.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">View Project</a>
@@ -26,7 +27,7 @@ const Portfolio: React.FC = () => {
 
           {/* Project 3: Svelte Music Visualizer */}
           <div className="project bg-gray-100 p-6 rounded-lg shadow-md">
-            <img src="/luces.png" alt="Svelte Music Visualizer Preview" className="mb-4 rounded"/>
+            <Image src="/luces.png" alt="Svelte Music Visualizer Preview" width={500} height={300} className="mb-4 rounded"/>
             <h2 className="text-2xl font-semibold mb-4">Svelte Music Visualizer</h2>
             <p className="mb-4 text-justify">A Svelte project presenting a matrix of colors that can be synchronized to the rhythm of your favorite song, creating a dynamic visualization. </p>
             <a href="https://box-dinamic.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">View Project</a>
