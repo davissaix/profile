ccd437f3745daee77adb47db2b99e26ba0dcc3fd
David AVILA ALVAREZ <davissaix@gmail.com>
2023-08-22 12:03:26 -0500

    2

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,11 @@ const Home: React.FC = () => {
             <h1 className="text-4xl font-bold">David Ávila</h1>
           </div>
 
-          <p className="mb-4 text-justify">Greetings! I'm <span className="font-semibold">David</span>, a web developer with a passion for creating dynamic digital experiences. Over the past two years, I've immersed myself in web development, mastering technologies such as JavaScript, React, and Next.js. My explorations extend to Python and cloud computing. I also bring a master's degree in Finance Management to the table, providing me with a unique perspective on projects.</p>
+          <p className="mb-4 text-justify">Greetings! I am <span className="font-semibold">David</span>, a web developer with a passion for creating dynamic digital experiences. Over the past two years, I've immersed myself in web development, mastering technologies such as JavaScript, React, and Next.js. My explorations extend to Python and cloud computing. I also bring a master's degree in Finance Management to the table, providing me with a unique perspective on projects.</p>
 
-          <p className="mb-4 text-justify">Outside of coding and finance, I'm a polyglot fluent in five languages (es, fr, en, pr, de). This linguistic ability underscores my love for diverse cultures and effective communication. And when screens don't captivate me, the streets do, through the lens of my camera, as I engage in street photography.</p>
+          <p className="mb-4 text-justify">Outside of coding and finance, I am a polyglot fluent in five languages (es, fr, en, pr, de). This linguistic ability underscores my love for diverse cultures and effective communication. And when screens don't captivate me, the streets do, through the lens of my camera, as I engage in street photography.</p>
 
-          <p className="text-justify">If you're searching for a full-stack developer with a diverse skill set, do take a moment to explore my portfolio. I'm open to opportunities and collaborations. Let's envision and bring to life something remarkable together!</p>
+          <p className="text-justify">If you are searching for a full-stack developer with a diverse skill set, do take a moment to explore my portfolio. I am open to opportunities and collaborations. Let's envision and bring to life something remarkable together!</p>
         </div>
       </div>
     </Layout>
