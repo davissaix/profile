8751019be8f2b635602c07390357feefcc752ba5
David AVILA ALVAREZ <davissaix@gmail.com>
2023-08-22 12:34:13 -0500

    quotes

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,9 +7,9 @@ const About: React.FC = () => {
       <div className="container mx-auto px-4 md:px-0 mt-10">
         <div className="bg-gray-100 p-6 rounded-lg shadow-md m-4">
           <h1 className="text-4xl font-bold mb-8 text-center">About Me</h1>
-          <p className="mb-4 text-justify">From getting to grips with finance to exploring the world of web development, my journey blends logic with creativity. Over five years in the stock market, I've honed my skills in Portfolio Management, Corporate Finance, and Data Analysis. With a Value Investing strategy guiding me, I've excelled in managing risk and making data-driven decisions</p>
-          <p className="mb-4 text-justify">Beyond the realm of web development, I have a keen interest in Data Science. I predominantly use Python, and I'm particularly captivated by language models, especially those like Lang Chain.</p>
-          <p className="mb-4 text-justify">Today, I find myself at the exciting crossroads of finance and technology. I am eager to join a team that values a unique blend of number-crunching prowess and coding. If you're seeking someone skilled in web development and programming, let's connect. Together, we can craft solutions that are both functional and innovative.</p>
+          <p className="mb-4 text-justify">From getting to grips with finance to exploring the world of web development, my journey blends logic with creativity. Over five years in the stock market, I&apos;ve honed my skills in Portfolio Management, Corporate Finance, and Data Analysis. With a Value Investing strategy guiding me, I&apos;ve excelled in managing risk and making data-driven decisions.</p>
+          <p className="mb-4 text-justify">Beyond the realm of web development, I have a keen interest in Data Science. I predominantly use Python, and I&apos;m particularly captivated by language models, especially those like Lang Chain.</p>
+          <p className="mb-4 text-justify">Today, I find myself at the exciting crossroads of finance and technology. I am eager to join a team that values a unique blend of number-crunching prowess and coding. If you&apos;re seeking someone skilled in web development and programming, let&apos;s connect. Together, we can craft solutions that are both functional and innovative.</p>
         </div>
       </div>
     </Layout>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,8 +7,8 @@ const Contact: React.FC = () => {
     <Layout>
       <div className="container mx-auto px-4 md:px-0 mt-10">
         <div className="bg-gray-100 p-6 rounded-lg shadow-md m-4">
-          <h1 className="text-4xl font-bold mb-8 text-center">Let's talk</h1>
-          <p className="mb-4 text-justify">If you're interested in collaborating or just want to chat, feel free to reach out! You can find more about my professional journey on the platforms below:</p>
+          <h1 className="text-4xl font-bold mb-8 text-center">Let&apos;s talk</h1>
+          <p className="mb-4 text-justify">If you&apos;re interested in collaborating or just want to chat, feel free to reach out! You can find more about my professional journey on the platforms below:</p>
         
           <div className="flex justify-center space-x-6 mt-4">
             {/* LinkedIn SVG Icon */}
